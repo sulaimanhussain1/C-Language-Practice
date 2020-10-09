4942781f9a3a13f96b618182e9b705fcd68a938b
Sulaiman Hussain <hussainsulaiman460@gmail.com>
2020-10-09 14:02:23 -0700

    added scanf.c file

---
+++ scanf.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+#include <stdlib.h>
+ int main()
+{
+    int a , b , c ,d ,sum;
+    printf("Enter a first value : ");
+    scanf("%d",&a);
+    printf("\nEnter a second value : ");
+    scanf("%d",&b);
+    printf("\nEnter a third value : ");
+    scanf("%d",&c);
+    printf("\nEnter a fourth value : ");
+    scanf("%d",&d);
+    sum = a+b+c+d;
+    printf("\nTotal sum is = %d ", sum);
+
+
+}
