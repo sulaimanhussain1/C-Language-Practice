ec0602c397fa4ae4f92f521c15e1c991cd08746d
Sulaiman Hussain <hussainsulaiman460@gmail.com>
2020-10-09 14:03:35 -0700

    added while-loop.c file

---
+++ while-loop.c
@@ -0,0 +1,14 @@
+// while loop program..
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i = 0;
+    while (i < 25)
+    {
+        printf("%d\n" , i + 1);
+        i = i + 1;
+
+    }
+    
+    return 0;
+}
