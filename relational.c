2bd220da6aba4b144204ad00e6b14383e3c57d94
Sulaiman Hussain <hussainsulaiman460@gmail.com>
2020-10-09 14:02:07 -0700

    added relational.c file

---
+++ relational.c
@@ -0,0 +1,19 @@
+// Relational Operators  //
+
+#include<stdio.h>
+main()
+{
+    int m = 5 , k = 10 ;
+    float p = 2.33;
+    double f = 6.556;
+    printf("*************************************\n");
+    printf("  ABOUT RELATIONAL OPERATORS   \n");
+    printf("**************************************\n\n");
+    printf("Print the value of an integer %d\n" , m);
+    printf("Print the value of float %f \n" , p);
+    printf("Print the value of double %f \n" , f+3);
+    printf("  %d Factorial is %d\n\n\n" , m , k);
+    printf( " %d %d %f %f \n\n\n\n" , m,k,p,f);
+    return (0);
+
+}
