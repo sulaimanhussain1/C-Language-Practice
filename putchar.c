55866a6a6a22722b23664ab77f3d03daaa6844ee
Sulaiman Hussain <hussainsulaiman460@gmail.com>
2020-10-09 14:00:58 -0700

    added putchar.c file

---
+++ putchar.c
@@ -0,0 +1,12 @@
+/* uses a function of putchar and putch. */
+#include<stdio.h>
+#include<conio.h>   //only for putch function..
+ main()
+{
+    char a = 'G';
+    putchar(a);
+    putchar('\n');
+    putchar('S');
+    putch('\n');
+    putch(a);
+}
